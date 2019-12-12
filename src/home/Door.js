c3a58492715ec9e4b830af5913ab789dec5d90a8
Adam Siwiec <brickmaster01@gmail.com>
2019-12-11 19:49:04 -0800

    temp

--- src/home/Door.js
+++ src/home/Door.js
@@ -15,7 +15,7 @@ const styles = {
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
 };
 
---
+++ src/onboard/Login.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+let Login = props => {
+  useEffect(() => {});
+
+  return <></>;
+};
